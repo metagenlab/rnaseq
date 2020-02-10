3ab08b203f045b1e425e22991d0f8b4a5c3cf8ec
tpillone <trestan.pillonel@gmail.com>
2020-02-10 10:51:26 +0100

    save the rpkm table, added FRD and logFC cutoffs as parameters

--- rules/scripts/edger_analysis.R
+++ rules/scripts/edger_analysis.R
@@ -12,9 +12,16 @@ split_path <- function(path) {
   return(c(basename(path), split_path(dirname(path))))
 }
 
+# cutoffs for filtering
+FDR_cutoff = snakemake@params$FDR_cutoff
+logFC_cutoff = snakemake@params$logFC_cutoff
+
+print("FRD_cutoff", FDR_cutoff)
+print(FDR_cutoff/2)
+
 # READ COUNT TABLE
 read_counts <- read.table(snakemake@input$count_file, header=T)
-head(read_counts )
+
 # extract conditions from output path
 path <- split_path(snakemake@output[[1]])
 cond1 <- strsplit(path[[2]], '_vs_')[[1]][[1]]
@@ -22,7 +29,6 @@ cond2 <- strsplit(path[[2]], '_vs_')[[1]][[2]]
 
 # READ custom_annotation table 
 custom_annotation_table <- read.csv(snakemake@params$annotation, header=T, as.is=T, stringsAsFactors = FALSE, sep="\t")
-head(custom_annotation_table)
 
 # READ SAMPLE TABLE
 sample_info_table <- read.table(snakemake@params$sample_table, header=T, as.is=T, stringsAsFactors = FALSE, sep="\t")
@@ -68,45 +74,47 @@ de <- exactTest(d, pair = c(cond2, cond1))
 tT <- topTags(de, n = nrow(d))
 # tabular form of differentially expressed genes 
 deg.list <- tT$table
-w <- which(tT$table$FDR <.01 & abs(tT$table$logFC) > 1)
+# cutoffs
+w <- which(tT$table$FDR < FDR_cutoff & abs(tT$table$logFC) > logFC_cutoff)
 tT$table$signif <- FALSE
 tT$table$signif[w] <- TRUE
 
 locus_tags <- rownames(deg.list)
 # select genes that have 10% false discovery rate
-top.deg <- locus_tags[deg.list$FDR < .01]
+top.deg <- locus_tags[deg.list$FDR < FDR_cutoff]
 
-## VOLCANO PLOT
+## VOLCANO PLOT: cutoffs of 0.01 FDR and 2 logFC
 pdf(snakemake@output[[2]])
   plot(deg.list$logFC, -log10(deg.list$FDR), # PValue
       pch=20, main=paste(cond1, "vs", cond2, "comparison"),
       xlab = "Log2 Fold Change", ylab = "-log10(FDR)",
       xlim=c(-max(abs(deg.list$logFC))*1.1, max(abs(deg.list$logFC))*1.1) )
-  with(subset(deg.list, FDR <.01 & abs(logFC)>2), 
+  with(subset(deg.list, FDR < .01 & abs(logFC) > 2), 
       points(logFC, -log10(FDR), pch=20, col="red"))
-  s <- subset(deg.list, FDR<.01 & abs(logFC)>2)
+  s <- subset(deg.list, FDR < .01 & abs(logFC) > 2)
   text(s$logFC, -log10(s$FDR), labels = paste(s$locus_tag, s$KO))
   abline(v=2, lty=2, lwd=1, col="lightblue")
   abline(v=-2, lty=2, lwd=1, col="lightblue")
 dev.off()
 
+## VOLCANO PLOT: cutoffs of FRD and logFC as defined in config file
 pdf(snakemake@output[[3]])
   plot(deg.list$logFC, -log10(deg.list$FDR), 
   pch=20, main=paste(cond1, "vs", cond2, "comparison"),
   xlab = "Log2 Fold Change", ylab = "-log10(FDR)", 
   xlim=c(-max(abs(deg.list$logFC))*1.1, max(abs(deg.list$logFC))*1.1))
-  with(subset(deg.list, FDR <.01 & abs(deg.list$logFC) > 1), 
+  with(subset(deg.list, FDR < FRD_cutoff & abs(deg.list$logFC) > logFC_cutoff), 
   points(logFC, -log10(FDR), pch=20, col="red"))
-  s <- subset(deg.list, FDR <.01 & abs(deg.list$logFC) > 1)
+  s <- subset(deg.list, FDR < FRD_cutoff & abs(deg.list$logFC) > logFC_cutoff)
   text(s$logFC, -log10(s$FDR), labels = paste(s$locus_tag, s$KO))
   abline(v=1, lty=2, lwd=1, col="lightblue")
   abline(v=-1, lty=2, lwd=1, col="lightblue")
 dev.off()
 
+## plot SMEAR
 pdf(snakemake@output[[4]])
 plotSmear(de, de.tags = top.deg, main = paste(cond1, "vs", cond2, "comparison"))
 dev.off()
-
 
 # Normalize counts
 pseudo_counts <- log2(y$counts + 1)
@@ -116,13 +124,13 @@ dfFilter <- data.frame(dfFilter, sample = dfFilter$variable)
 m <- match(dfFilter$sample, sample_info_table$OldSampleName)
 dfFilter$condition <- sample_info_table$condition[m]
 
-pdf("psuedocounts.pdf")
+pdf("pseudocounts.pdf")
 ggplot(dfFilter, aes(x = value, colour = variable, fill = variable)) +
   geom_histogram(binwidth = 0.6) + facet_wrap(~ condition) +
   theme(legend.position = "top") + xlab("log2(counts)") + ggtitle("log2(counts) distribution")
 dev.off()
 
-pdf("psuedocounts_boxplots.pdf")
+pdf("pseudocounts_boxplots.pdf")
 ggplot(dfFilter, aes(x=sample, y=value)) + 
   geom_boxplot(outlier.colour="red", outlier.shape=8, outlier.size=4)+ facet_wrap(~ condition)
 dev.off()
@@ -156,6 +164,12 @@ metadata_gene$up_or_down <- as.factor(metadata_gene$up_or_down)
 
 w <- length(considered_samples)*2.5 + nchar(max(rownames(log_rpkm.topgenes))) / 10
 
+
+#################################
+###  top 100 genes heatmap ######
+#################################
+
+
 svglite(snakemake@output[[5]], height=23, width=w)
 pheatmap(log_rpkm.topgenes[,considered_samples], 
          cellwidth = 12, 
@@ -164,29 +178,33 @@ pheatmap(log_rpkm.topgenes[,considered_samples],
          main = paste('Heatmap of top 100 genes:', cond1, " vs ", cond2)) # , annotation_col=my_sample_col
 dev.off()
 
-# WRITE table
+#################################
+###  Write table ################
+#################################
+
 write.table(tT, snakemake@output[[6]], sep="\t")
 
+# filter according to config file cutoffs for FRD and logFC
+w <- which(tT$table$FDR < FRD_cutoff & abs(tT$table$logFC) > logFC_cutoff)
 
-w <- which(tT$table$FDR <.01 & abs(tT$table$logFC) > 1)
 locus_list <- tT$table$locus_tag[w] 
 genes_rpkm_filtered <- as.data.frame(genes_rpkm[locus_list,])
+genes_rpkm_filtered$logFC <- tT$table$logFC[w] 
 
+# prepare consensus annotation for each gene
 m <- match(rownames(genes_rpkm_filtered), custom_annotation_table$locus_tag)
-
 gene <- custom_annotation_table$gene[m]
 product <- custom_annotation_table$product[m]
 annot <- paste0(rownames(genes_rpkm_filtered), '/', gene, '/', product)
 rownames(genes_rpkm_filtered) <- annot
 
-print("genes_rpkm_filtered")
-head(genes_rpkm_filtered)
-genes_rpkm_filtered$logFC <- tT$table$logFC[w] 
-head(genes_rpkm_filtered)
 # order by the absolute value of fold change
 genes_rpkm_filtered_ordered <- genes_rpkm_filtered[order(-abs(genes_rpkm_filtered$logFC)),]
 
-###  top 100 downregulated  ###
+#################################
+###  top 100 downregulated  #####
+#################################
+
 # order by logFC
 genes_rpkm_filtered_ordered <- genes_rpkm_filtered_ordered[order(genes_rpkm_filtered_ordered$logFC),]
 # keep only downregulated genes 
@@ -209,7 +227,10 @@ pheatmap(log2(genes_rpkm_filtered_ordered_down[,considered_samples] + 1),
 dev.off()
 
 
-###  top 100 upregulated  ###
+#################################
+###  top 100 upregulated  #######
+#################################
+
 # order by logFC
 genes_rpkm_filtered_ordered <- genes_rpkm_filtered_ordered[order(-genes_rpkm_filtered_ordered$logFC),]
 # keep only upregulated genes
