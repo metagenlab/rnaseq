ecd4e8fd57b2a78d42d8b6c2ccd52fd5265cd784
Trestan Pillonel <trestan.pillonel@gmail.com>
2024-02-01 08:41:33 +0100

    deal with trna and rrna

--- rules/scripts/rpkm.R
+++ rules/scripts/rpkm.R
@@ -16,12 +16,24 @@ sample_info_table[,"total_reads"] <- NA
 
 # read reference gff
 annotations <- readGFF(snakemake@input$reference_gff)
-annotations <- annotations[annotations$type == 'gene',]
-rownames(annotations) <- annotations$locus_tag
+annotations <- annotations[annotations$type %in% c('gene', 'pseudogene'),]
+annotations <- annotations[annotations$gene_biotype != 'rRNA' ,]
+annotations <- annotations[annotations$gene_biotype != 'tRNA' ,]
+annotations <- as.data.frame(annotations)
+
+start_lst <- aggregate(annotations$start, by = list(annotations$locus_tag), min)
+end_lst <- aggregate(annotations$end, by = list(annotations$locus_tag), max)
+
+annotations <- annotations[!duplicated(annotations$locus_tag), ]
+annotations$start <- start_lst$x
+annotations$end <- end_lst$x
 
 m <- match(annotations$locus_tag, custom_annotation_table$locus_tag)
 
 annotations <- cbind(annotations, custom_annotation_table[m,])
+
+rownames(annotations) <- annotations$locus_tag
+
 
 # COUNT table
 y <- DGEList(counts=read_counts, 
@@ -31,11 +43,15 @@ y <- DGEList(counts=read_counts,
 
 # add gene length to table
 annotations$gene_length <- (annotations$end - annotations$start) + 1
+print(rownames(annotations))
 m <- match(rownames(y$genes), rownames(annotations))
+
 y$genes$length <- annotations$gene_length[m]
 
 # claculated RPKM
-genes_rpkm_raw <- as.data.frame(rpkm(y))
+genes_rpkm_raw <- rpkm(y)
+
+genes_rpkm_raw <- as.data.frame(genes_rpkm_raw)
 
 genes_rpkm_raw <- cbind(locus_tag=rownames(genes_rpkm_raw), genes_rpkm_raw)
 
