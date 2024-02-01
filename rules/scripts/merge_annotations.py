7fb3f3910e6d2b68b44fb52b3e654ff8ee2eb297
Trestan Pillonel <trestan.pillonel@gmail.com>
2024-02-01 08:40:39 +0100

    added COG annot as rule input

--- rules/scripts/merge_annotations.py
+++ rules/scripts/merge_annotations.py
@@ -5,8 +5,9 @@ from Bio import SeqIO
 reference_gbk = snakemake.input[0] # reference_genome.gff
 reference_genome_uniprotKB_ko = snakemake.input[1] # reference/reference_genome_uniprotKB_ko.txt
 ko_description = snakemake.input[2] # reference/ko_description.txt
-reference_genome_uniprotKB_COG = snakemake.input[3] # reference/reference_genome_uniprotKB_COG.txt
-COG_annotations = snakemake.input[4] # reference/COG_annotations.tsv"
+#reference_genome_uniprotKB_COG = snakemake.input[3] # reference/reference_genome_uniprotKB_COG.txt
+COG_annotations = snakemake.input[3] # reference/COG_annotations.tsv"
+COG_names = snakemake.input[4]
 o = open(snakemake.output[0], 'w')
 
 header = ["locus_tag",	
@@ -23,9 +24,11 @@ locus_tag2ko = pandas.read_csv(reference_genome_uniprotKB_ko,
                            delimiter='\t',
                            names=["locus_tag","ko"]).set_index("locus_tag").to_dict()["ko"]
 
-locus_tag2cog = pandas.read_csv(reference_genome_uniprotKB_COG,
+locus_tag2cog = pandas.read_csv(COG_annotations,
                            delimiter='\t',
                            names=["locus_tag","cog"]).set_index("locus_tag").to_dict()["cog"]
+
+print(locus_tag2cog)
 
 ko2description = pandas.read_csv(ko_description,
                            delimiter='\t',
@@ -38,9 +41,12 @@ for ko in list(ko2description.keys()):
     if ko != ko_edit:
         ko2description[ko_edit] = ko2description[ko]
 
-cog2description = pandas.read_csv(COG_annotations,
+cog2description = pandas.read_csv(COG_names,
                            delimiter='\t',
-                           names=["cog","category","description"]).set_index("cog").to_dict()["description"]
+                           names=["cog","category","description"],comment="#").set_index("cog").to_dict()["description"]
+
+print(cog2description)
+
 
 o.write("\t".join(header)+'\n')
 
@@ -73,7 +79,7 @@ for rec in SeqIO.parse(reference_gbk, "genbank"):
             except:
                 cog = ''
                 cog_description = '-'
-            o.write(f'{locus_tag}\t{gene}\t{product}\t{cog_description}\t{ko}\t{ko_description}\t-\t-\n')
+            o.write(f'{locus_tag}\t{gene}\t{product}\t{cog}\t{cog_description}\t{ko}\t{ko_description}\t-\t-\n')
         if feature.type in ["tRNA", "rRNA", "ncRNA"]:
             try:
                 locus_tag = feature.qualifiers["locus_tag"][0]
